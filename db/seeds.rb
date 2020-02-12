312fa01b7530360ac741c2ab217fe39fa019d3f5
Alexandre FRASCHINI <alexandre.fraschini@gmail.com>
2020-02-12 12:43:19 +0100

    seeds 5 restaurants

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,37 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts "Cleaning database..."
+Restaurant.destroy_all
+
+puts "Creating 5 restaurants..."
+restaurants_attributes = [
+  {
+    name: "Burger Empire",
+    address: "7 Boundary St, London E2 7JE",
+    category: "french"
+  },
+  {
+    name: "Mc Donald's",
+    address: "523 Shitty St, London",
+    category: "belgian"
+  },
+  {
+    name: "Pizza Nico",
+    address: "110 Cours Balguerie, Bordeaux",
+    category: "italian"
+  },
+  {
+    name: "Rake!",
+    address: "666 Rspec Avenue, New York City",
+    category: "french"
+  },
+  {
+    name: "United UBB",
+    address: "432 Rugby Bd, Bordeaux",
+    category: "french"
+  }
+]
+Restaurant.create!(restaurants_attributes)
+puts "Finished!"
